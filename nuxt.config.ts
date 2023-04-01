2e00423c7274a8bbc1a907aac7f62a8b4a04ddc3
Aleksey Razbakov <aleksey@razbakov.com>
2023-04-01 13:19:45 +0200

    disable @sidebase/nuxt-auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@sidebase/nuxt-auth',
     '@nuxtjs/tailwindcss',
     '@huntersofbook/naive-ui-nuxt'
   ],
