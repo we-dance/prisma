aedfc8397ef6bce3631a57d4ed5e1955181cee4d
Mr. Robot <gomexxxx71@gmail.com>
2023-05-12 00:15:05 +0600

    initial commit.

--- server/api/events/index.ts
+++ server/api/events/index.ts
@@ -1,7 +1,27 @@
 import { getQuery } from 'h3'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+async function findEventsWithinDistance(city:string, lng:number, lat:number, distanceInKm:number) {
+    const events = await prisma.$queryRaw`
+      SELECT * 
+      FROM events 
+      WHERE ST_Distance_Sphere(
+        ST_MakePoint(${lng}, ${lat}),
+        ST_MakePoint(venues_longitude, venues_latitude)
+      ) <= ${distanceInKm * 1000}
+      AND venues.city = ${city}
+    `
+    return events; 
+}
+
 
 export default defineEventHandler((event) => {
   const { username } = getQuery(event)
+
+
 
   return event.context.prisma.event.findMany({
     where: {
