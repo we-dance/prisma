75502949d9e9669b516c0914f2e80d6fdc18a4dd
Mr. Robot <gomexxxx71@gmail.com>
2023-05-18 23:17:23 +0600

    add postgis

--- server/api/events/index.ts
+++ server/api/events/index.ts
@@ -4,46 +4,23 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 
-async function findEventsWithinDistance(city:string, lng:number, lat:number, distanceInKm:number) {
-    const events = await prisma.$queryRaw`
-      SELECT * 
-      FROM events 
+export default defineEventHandler(async (event) => {
+  
+  const { lng, lat, distance } = getQuery(event)
+ 
+  const events = await prisma.$queryRaw`
+    SELECT *
+    FROM "Event"
+    WHERE venueId IN (
+      SELECT id
+      FROM "Profile"
       WHERE ST_Distance_Sphere(
         ST_MakePoint(${lng}, ${lat}),
-        ST_MakePoint(venues_longitude, venues_latitude)
-      ) <= ${distanceInKm * 1000}
-      AND venues.city = ${city}
-    `
-    return events; 
-}
+        ST_MakePoint(lng, lat)
+      ) <= ${(distance as number ?? 50) * 1000}
+    );
+  `;
 
-
-export default defineEventHandler((event) => {
-  const { username } = getQuery(event)
-
-
-
-  return event.context.prisma.event.findMany({
-    where: {
-      startDate: {
-        gte: new Date()
-      },
-      venue: {
-        city: {
-          username
-        }
-      }
-    },
-    include: {
-      venue: true,
-      organizer: true,
-      styles: true
-    },
-    orderBy: [
-      {
-        startDate: 'asc'
-      }
-    ],
-    take: 10
-  })
+  return events; 
+  
 })
