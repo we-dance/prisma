f97c3dba03bcd1895c96bb2c3ec08785d2108a67
Mr. Robot <gomexxxx71@gmail.com>
2023-06-15 22:13:54 +0600

    fix: add new line

--- formkit.config.ts
+++ formkit.config.ts
@@ -1,26 +1,30 @@
-import { generateClasses } from '@formkit/themes'
+import { generateClasses } from "@formkit/themes";
 
 export default {
   config: {
-    classes: generateClasses({ 
-      global:{
-        label: 'block mb-1 font-bold text-sm',
-        outer:'my-5',
-        message: 'text-red-500 mb-1 text-sm',
-        messages: 'list-none p-0 mt-1 mb-0',
+    classes: generateClasses({
+      global: {
+        label: "block mb-1 font-bold text-sm",
+        outer: "my-5",
+        message: "text-red-500 mb-1 text-sm",
+        messages: "list-none p-0 mt-1 mb-0",
       },
-      text:{
-        input:'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline', 
-      }, 
+      text: {
+        input:
+          "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+      },
       textarea: {
-        input:'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline', 
-      }, 
+        input:
+          "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+      },
       date: {
-        input:'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline', 
-      }, 
-      submit:{
-        input:'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-      }
-    })
-  }
-}
+        input:
+          "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+      },
+      submit: {
+        input:
+          "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline",
+      },
+    }),
+  },
+};
